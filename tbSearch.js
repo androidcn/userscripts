e8ce623acaa1652b83f2bf04b27a581dd45b9208
androidcn <flatlandcn@gmail.com>
2023-08-19 18:24:59 +0800

    Update tbSearch.js

--- tbSearch.js
+++ tbSearch.js
@@ -1,9 +1,9 @@
 var body = JSON.parse($response.body); 
- var allSections =  body.data.itemArrays;
+ var allSections =  body.data.itemsArray;
  allSections = allSections.filter((item) => { 
          return !item.utLogMap.hasOwnProperty("ad_slot");
  }) 
- body.data.itemArrays = allSections; 
+ body.data.itemsArray = allSections; 
  //body.data.feedsCount = allSections.length; 
  body = JSON.stringify(body); 
  console.log("已删除TB搜索广告"); 
