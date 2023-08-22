a4ca86109294ed1f0a4f70ea47f28c186e91d194
androidcn <flatlandcn@gmail.com>
2023-08-22 15:22:54 +0800

    Create tonghuashun.js

---
+++ tonghuashun.js
@@ -0,0 +1,24 @@
+const url =$request.url;
+if(!$response.body)
+{
+ $done({});
+}
+var body = JSON.parse($response.body); 
+if (url.includes("recommend"))
+{
+var allSections =  body.data;
+ allSections = allSections.filter((item) => { 
+         return item.info.subject != “ad”;
+ }) 
+ body.data = allSections; 
+ //body.data.feedsCount = allSections.length; 
+ body = JSON.stringify(body); 
+ console.log("已删除tonghuashun广告");  
+$done({ 
+     body 
+ });
+}
+else{
+ $done({});
+}
+ 
