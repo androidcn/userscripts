7de25b1b8ff8ba8365763d45ab21909e8d1071f3
androidcn <flatlandcn@gmail.com>
2024-03-12 09:37:43 +0800

    Update Twiiter.js

--- Twiiter.js
+++ Twiiter.js
@@ -2,11 +2,12 @@
 // @name         Make Twitter Great Again
 // @namespace    https://github.com/androidcn/userscripts/
 // @version      2024-03-11
-// @description  Frome Chrome Extension "Make Twitter Great Again" not interesting this post
+// @description  为Twitter增加两个按钮，快速让Twitter算法知道你“不感兴趣的推文“ From Chrome Extension "Make Twitter Great Again" not interesting this post
 // @author       theopenprojects.io
 // @match        https://twitter.com/home
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 const silencePath = 'path[d="M18 6.59V1.2L8.71 7H5.5C4.12 7 3 8.12 3 9.5v5C3 15.88 4.12 17 5.5 17h2.09l-2.3 2.29 1.42 1.42 15.5-15.5-1.42-1.42L18 6.59zm-8 8V8.55l6-3.75v3.79l-6 6zM5 9.5c0-.28.22-.5.5-.5H8v6H5.5c-.28 0-.5-.22-.5-.5v-5zm6.5 9.24l1.45-1.45L16 19.2V14l2 .02v8.78l-6.5-4.06z"]'
