2c32eec5b0005aa491c99a8cb6ce34846135f3f1
androidcn <flatlandcn@gmail.com>
2026-05-13 12:50:49 +0800

    Update Twitter.user.js
    适配全局-快速拉黑和屏蔽垃圾人

--- Twitter.user.js
+++ Twitter.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Make Twitter Great Again
 // @namespace    https://github.com/androidcn/userscripts/
-// @version      2024-12-25
+// @version      2026-05-13
 // @description  为Twitter增加两个按钮，快速让Twitter算法知道你“不感兴趣的推文“ From Chrome Extension "Make Twitter Great Again" not interesting this post
 // @author       theopenprojects.io
-// @match        https://twitter.com/home
-// @match        https://x.com/home
+// @match        https://twitter.com/*
+// @match        https://x.com/*
 // @updateURL       https://github.com/androidcn/userscripts/raw/main/Twitter.user.js
 // @downloadURL     https://github.com/androidcn/userscripts/raw/main/Twitter.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
